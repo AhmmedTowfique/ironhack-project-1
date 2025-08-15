2084b036b2056dc13b8fbf565849c3a04313618e
AhmmedTowfique <towfique@Towfique-ThinkPad.localdomain>
2025-08-15 10:11:31 +0200

    Prepare project for local run: updated app.py, Program.cs, added .gitignore

--- worker/Program.cs
+++ worker/Program.cs
@@ -17,12 +17,12 @@ namespace Worker
             try
             {
                 // Fetch configuration from environment variables
-                var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "db";
+                var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
                 var dbUsername = Environment.GetEnvironmentVariable("DB_USERNAME") ?? "postgres";
                 var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "postgres";
                 var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "postgres";
                 
-                var redisHost = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "redis";
+                var redisHost = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "localhost";
 
                 // Construct the connection strings
                 var pgConnectionString = $"Server={dbHost};Username={dbUsername};Password={dbPassword};Database={dbName}";
